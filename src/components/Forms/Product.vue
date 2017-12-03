9ccc1e7f109fec57317cd763d7e3ca175ae26423
Gustavo Castilião <gucastiliao@gmail.com>
2017-12-03 15:45:47 -0200

    add component to create/edit variations and separate from product form

--- src/components/Forms/Product.vue
+++ src/components/Forms/Product.vue
@@ -1,105 +1,65 @@
 <template>
-  <div class="ui segment left aligned">
-    <form class="ui form" @submit.prevent="save" @keydown="product.errors.clear($event.target.name)">
-      <div class="ui grid">
-        <div class="four wide column">
-          <div class="ui secondary vertical menu">
-            <a class="item" v-on:click="page = 'general'">
-              {{ $t('General', {locale: getLocale()}) }}
-            </a>
-            <a class="item" v-on:click="page = 'variations'">
-              {{ $t('Variations', {locale: getLocale()}) }}
-            </a>
-          </div>
+  <form class="ui form" @submit.prevent="save" @keydown="product.errors.clear($event.target.name)">
+    <div v-if="page == 'general'">
+      <div class="two fields">
+        <div class="field">
+          <label>SKU</label>
+          <input :disabled="method == 'patch'" type="text" name="sku" placeholder="SKU" v-model="product.sku" :class="{ 'is-invalid': product.errors.has('sku') }">
+          <has-error class="ui negative message" :form="product" field="sku"></has-error>
         </div>
-        
-        <div v-if="page === 'general'" style="width: 75%;margin: 20px -40px;">
-          <div class="twelve wide column">
-            <div class="two fields">
-              <div class="field">
-                <label>SKU</label>
-                <input :disabled="method == 'patch'" type="text" name="sku" placeholder="SKU" v-model="product.sku" :class="{ 'is-invalid': product.errors.has('sku') }">
-                <has-error class="ui negative message" :form="product" field="sku"></has-error>
-              </div>
-              <div class="field">
-                <label>{{ $t('Name', {locale: getLocale()}) }}</label>
-                <input type="text" name="name" :placeholder="$t('Name', {locale: getLocale()})  " v-model="product.name" :class="{ 'is-invalid': product.errors.has('name') }">
-                <has-error class="ui negative message" :form="product" field="name"></has-error>
-              </div>
-            </div>
-            <div class="two fields">
-              <div class="field">
-                <label>{{ $t('Price', {locale: getLocale()}) }}</label>
-                <div class="ui labeled input">
-                  <div class="ui label">$</div>
-                  <input type="text" name="price" :placeholder="$t('Price', {locale: getLocale()})  " v-model="product.price" :class="{ 'is-invalid': product.errors.has('price') }">
-                </div>
-                <has-error class="ui negative message" :form="product" field="price"></has-error>
-              </div>
-              <div class="field">
-                <label>{{ $t('Stock', {locale: getLocale()}) }}</label>
-                <input type="text" name="stock" :placeholder="$t('Stock', {locale: getLocale()})" v-model="product.stock" :class="{ 'is-invalid': product.errors.has('stock') }">
-                <has-error class="ui negative message" :form="product" field="stock"></has-error>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div v-if="page === 'variations'" style="width: 75%;margin: 20px -40px;">
-          <div class="two fields">
-            <div class="field">
-              <label>SKU</label>
-              <input :disabled="method == 'patch'" type="text" name="sku" placeholder="SKU" v-model="product.variations[0].sku" :class="{ 'is-invalid': product.errors.has('sku') }">
-              <has-error class="ui negative message" :form="product" field="sku"></has-error>
-            </div>
-            <div class="field">
-              <label>{{ $t('Name', {locale: getLocale()}) }}</label>
-              <input type="text" name="name" :placeholder="$t('Name', {locale: getLocale()})  " v-model="product.variations[0].name" :class="{ 'is-invalid': product.errors.has('name') }">
-              <has-error class="ui negative message" :form="product" field="name"></has-error>
-            </div>
-          </div>
-          <div class="two fields">
-            <div class="field">
-              <label>{{ $t('Price', {locale: getLocale()}) }}</label>
-              <div class="ui labeled input">
-                <div class="ui label">$</div>
-                <input type="text" name="price" :placeholder="$t('Price', {locale: getLocale()})  " v-model="product.variations[0].price" :class="{ 'is-invalid': product.errors.has('price') }">
-              </div>
-              <has-error class="ui negative message" :form="product" field="price"></has-error>
-            </div>
-            <div class="field">
-              <label>{{ $t('Stock', {locale: getLocale()}) }}</label>
-              <input type="text" name="stock" :placeholder="$t('Stock', {locale: getLocale()})" v-model="product.variations[0].stock" :class="{ 'is-invalid': product.errors.has('stock') }">
-              <has-error class="ui negative message" :form="product" field="stock"></has-error>
-            </div>
-          </div>
-
-          <hr style="border-color: #ebebeb;margin: 25px 0px;">
+        <div class="field">
+          <label>{{ $t('Name', {locale: getLocale()}) }}</label>
+          <input type="text" name="name" :placeholder="$t('Name', {locale: getLocale()})  " v-model="product.name" :class="{ 'is-invalid': product.errors.has('name') }">
+          <has-error class="ui negative message" :form="product" field="name"></has-error>
         </div>
       </div>
-
-      <div class="ui grid">
-        <div class="sixteen wide column">
-          <button class="ui right floated info button" v-if="page === 'variations'"><i class="add circle icon"></i> {{ $t('Add Variation', {locale: getLocale()}) }}</button>
-
-          <button class="ui right floated primary button" :class="{ loading: product.busy }" :disabled="product.busy" type="submit">{{ $t('Save', {locale: getLocale()}) }}</button>
+      <div class="two fields">
+        <div class="field">
+          <label>{{ $t('Price', {locale: getLocale()}) }}</label>
+          <div class="ui labeled input">
+            <div class="ui label">$</div>
+            <input type="text" name="price" :placeholder="$t('Price', {locale: getLocale()})  " v-model="product.price" :class="{ 'is-invalid': product.errors.has('price') }">
+          </div>
+          <has-error class="ui negative message" :form="product" field="price"></has-error>
+        </div>
+        <div class="field">
+          <label>{{ $t('Stock', {locale: getLocale()}) }}</label>
+          <input type="text" name="stock" :placeholder="$t('Stock', {locale: getLocale()})" v-model="product.stock" :class="{ 'is-invalid': product.errors.has('stock') }">
+          <has-error class="ui negative message" :form="product" field="stock"></has-error>
         </div>
       </div>
-    </form>
-  </div>
+    </div>
+
+    <variations-form
+      :product-form="product"
+      method="patch"
+      v-if="page == 'variations'"
+      v-on:variations-updated="newVariations => product.variations = newVariations"
+    ></variations-form>
+
+    <div class="ui grid">
+      <div class="sixteen wide column">
+        <button class="ui right floated primary button" :class="{ loading: product.busy }" :disabled="product.busy" type="submit">{{ $t('Save', {locale: getLocale()}) }}</button>
+      </div>
+    </div>
+  </form>
 </template>
 
 <script>
 import { Form, HasError, AlertError } from 'vform'
+import VariationsForm from '../Forms/Variations'
 
 export default {
   name: 'productform',
   components: {
     HasError,
-    AlertError
+    AlertError,
+    VariationsForm
   },
   props: [
     'productData',
-    'method'
+    'method',
+    'page'
   ],
   data () {
     return {
@@ -108,25 +68,17 @@ export default {
         name: '',
         price: '',
         stock: '',
-        variations: [
-          {
-            sku: '',
-            name: '',
-            price: '',
-            stock: ''
-          }
-        ]
-      }),
-      page: 'general'
+        variations: []
+      })
     }
   },
   mounted () {
     if (this.productData) {
-      console.log(this.productData.variations)
       this.product.sku = this.productData.sku
       this.product.name = this.productData.name
       this.product.price = this.productData.price
       this.product.stock = this.productData.stock
+      this.product.variations = (this.productData.variations) ? this.productData.variations : []
     }
   },
   methods: {
@@ -135,7 +87,6 @@ export default {
     },
     save () {
       const url = (this.method === 'post') ? `/v1/products` : `/v1/products/${this.product.sku}`
-
       this.product[this.method](url)
         .then(({ data }) => {
           this.$toastr('add', {
@@ -146,7 +97,9 @@ export default {
             type: 'success'
           })
 
-          this.$router.push({ name: 'Products' })
+          if (this.method === 'post') {
+            this.$router.push({ name: 'Products' })
+          }
         })
     }
   }

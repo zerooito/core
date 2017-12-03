cfe9e73b7e0db867d3cc352db7c5a7daf08c85a2
Reginaldo Junior <76regi@gmail.com>
2017-12-03 19:20:43 -0200

    fix feature variation when create product

--- src/components/Products/Create.vue
+++ src/components/Products/Create.vue
@@ -1,13 +1,43 @@
 <template>
-  <product-form method="post"></product-form>
+  <div class="ui segment left aligned">
+    <div class="ui grid">
+      <div class="four wide computer sixteen wide mobile column product-menu">
+        <div class="ui secondary vertical menu">
+          <a class="item" v-on:click="page = 'general'" v-bind:class="{ active: page == 'general' }">
+            {{ $t('General', {locale: getLocale()}) }}
+          </a>
+          <a class="item" v-on:click="page = 'variations'" v-bind:class="{ active: page == 'variations' }">
+            {{ $t('Variations', {locale: getLocale()}) }}
+          </a>
+        </div>
+      </div>
+
+      <div class="twelve wide computer sixteen wide mobile column">
+        <product-form
+          :product-data="this.$route.params.product"
+          method="post"
+          :page="page"
+        ></product-form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import ProductForm from '../Forms/Product'
-
 export default {
   components: {
     ProductForm
+  },
+  data () {
+    return {
+      page: 'general'
+    }
+  },
+  methods: {
+    getLocale () {
+      return process.env.LOCALE
+    }
   }
 }
 </script>
